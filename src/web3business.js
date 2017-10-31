fea1629bdbb801d902602cd6f0e6671f25e24548
Phisit Kannasut <phisit@readyplanet.com>
2017-10-31 15:00:25 +0700

    add web3business

---
+++ src/web3business.js
@@ -0,0 +1,25 @@
+'use strict'
+var BUSINESS = function(_web3){
+  this.web3 = _web3; 
+  return this;
+}
+
+BUSINESS.prototype.setWeb3 = function(_web3){
+  this.web3 = _web3; 
+  return this;
+}
+
+BUSINESS.prototype.getBusinessContract = function(businessToken){
+  return new Promise(function(resolve,reject){
+    if(!this.web3) return reject('web3 not set');
+    var businessContract = {
+      address : '',
+      abiInterface : ''
+    }
+
+    
+    return resolve(businessContract);
+  })
+}
+
+module.exports = BUSINESS;
