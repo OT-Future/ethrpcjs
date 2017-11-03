b01dbd860831c80c15b33b5c89498dd9d0ea2549
Phisit Kannasut <phisit@readyplanet.com>
2017-11-03 18:02:24 +0700

    update business

--- src/web3business.js
+++ src/web3business.js
@@ -7,17 +7,17 @@ var abi = {
 
 var BUSINESS = function(parent) {
   this._parent = parent;
-  this.web3business = this._parent.web3.getModule();
+  this.web3 = this._parent.web3.getModule();
   this.abiTemplate = {
     ReadyERC20: require('./ReadyERC20.json')
   };
 
-  this.contractInterface = new this.web3business.eth.Contract(
+  this.contractInterface = new this.web3.eth.Contract(
     businessFactory.interface,
     businessFactory.address
   );
 
-  console.log('contractInterface', this.contractInterface);
+  //console.log('contractInterface', this.contractInterface);
 
   return this;
 };
@@ -29,7 +29,7 @@ BUSINESS.prototype.getBusinessContract = function(
   var _this = this;
 
   return new Promise(function(resolve, reject) {
-    if (!_this.web3business) return reject('web3 not set');
+    if (!_this.web3) return reject('web3 not set');
     var businessContract = {
       address: '',
       abiInterface: ''
@@ -37,11 +37,11 @@ BUSINESS.prototype.getBusinessContract = function(
     _this.contractInterface.methods.OwnedBusiness(businessToken, businessPosition).call().then(function(data) {
         var abiType = data[4].replace('.json','');
         data.abi = _this.abiTemplate[abiType];
-        var buzContract = new _this.web3business.eth.Contract(data.abi,data[3]);      
-        console.log('BUSINESS.prototype.getBusinessContract data', buzContract);
+        var buzContract = new _this.web3.eth.Contract(data.abi,data[3]);      
+        //console.log('BUSINESS.prototype.getBusinessContract data', buzContract);
         return resolve(buzContract);
       }).catch(function(error) {
-        console.log('BUSINESS.prototype.getBusinessContract error', error);
+        //console.log('BUSINESS.prototype.getBusinessContract error', error);
         return reject(error);
       });
   });
